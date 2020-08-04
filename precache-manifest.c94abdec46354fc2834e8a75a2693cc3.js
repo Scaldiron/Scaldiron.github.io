a61e754dfd76ff5f98d5216cb1be09fe8fe6bbc2
Skyler <swillkeough@gmail.com>
2020-08-04 01:46:25 -0500

    Updates

---
+++ precache-manifest.c94abdec46354fc2834e8a75a2693cc3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a7a2340764af0922ccdfe0a543dd03",
+    "url": "/scaldiron/index.html"
+  },
+  {
+    "revision": "e49f992f440553945d8b",
+    "url": "/scaldiron/static/css/main.b0aaccbd.chunk.css"
+  },
+  {
+    "revision": "d15825010fd21075f51e",
+    "url": "/scaldiron/static/js/2.97a1b65d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scaldiron/static/js/2.97a1b65d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49f992f440553945d8b",
+    "url": "/scaldiron/static/js/main.5ea5b0df.chunk.js"
+  },
+  {
+    "revision": "7b8baeb02a46422d2bc3",
+    "url": "/scaldiron/static/js/runtime-main.4cddaa67.js"
+  },
+  {
+    "revision": "c57ff5192ce9de77f0a69863d7a45b48",
+    "url": "/scaldiron/static/media/LifeEasier.c57ff519.png"
+  },
+  {
+    "revision": "8b9ebbc8397a92ac928af34418618e86",
+    "url": "/scaldiron/static/media/Magnolia.8b9ebbc8.png"
+  },
+  {
+    "revision": "fba3efd3d4ed48dcf23f0e79e978f8a0",
+    "url": "/scaldiron/static/media/Partnerships.fba3efd3.png"
+  },
+  {
+    "revision": "887e38a48db4bafa4babf2d2d452d33f",
+    "url": "/scaldiron/static/media/bernard.887e38a4.jpg"
+  },
+  {
+    "revision": "d793aeefc45e480b9b98af2b0d33f1fb",
+    "url": "/scaldiron/static/media/lucas.d793aeef.jpg"
+  },
+  {
+    "revision": "a862a23b078c2c3fb79eeaef3be12d3d",
+    "url": "/scaldiron/static/media/mikeh.a862a23b.jpg"
+  },
+  {
+    "revision": "10733a1b73e24014b8d20e391feabae7",
+    "url": "/scaldiron/static/media/olga.10733a1b.jpg"
+  },
+  {
+    "revision": "cd1f08fa4909f555617e2cca01b09297",
+    "url": "/scaldiron/static/media/skyler.cd1f08fa.jpg"
+  },
+  {
+    "revision": "50416e5760721ff37b8e230a9eecba04",
+    "url": "/scaldiron/static/media/tom.50416e57.jpg"
+  }
+]);
